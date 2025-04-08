0ff2d0df10948170d513437ff6ae14a57cdae094
hwrnr <hawerner.hawerner@gmail.com>
2025-04-08 17:32:22 +0200

    Enable autoinstall for nvim-treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,18 +1,19 @@
 local M = {
-    "nvim-treesitter/nvim-treesitter",
-    build = function()
-        require("nvim-treesitter.install").update({ with_sync = true })()
-    end,
-     config = function ()
-      local configs = require("nvim-treesitter.configs")
+  "nvim-treesitter/nvim-treesitter",
+  build = function()
+    require("nvim-treesitter.install").update({ with_sync = true })()
+  end,
+  config = function()
+    local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "python", "typescript", "json", "yaml", "css", "scss", "tsx", "graphql", "jsonc", "rust", "ruby", "go", "java", "php", "bash", "dockerfile", "hcl", "lua", "yaml"},
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },
-        })
-    end
+    configs.setup({
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "python", "typescript", "json", "yaml", "css", "scss", "tsx", "graphql", "jsonc", "rust", "ruby", "go", "java", "php", "bash", "dockerfile", "hcl", "lua", "yaml", "comment" },
+      auto_install = true,
+      sync_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end
 }
 
 return { M }
